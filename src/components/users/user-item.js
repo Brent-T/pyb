bef9138733cb7617f8d5b3955a2ccdd4399fe1d9
Brent-T <brenttimmermans7@gmail.com>
2018-04-21 15:48:22 +0200

    🎨 add general styling

--- src/components/users/user-item.js
+++ src/components/users/user-item.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import TimePicker from '../common/time-input';
 
+const containerStyles = {
+  display: 'flex',
+  alignItems: 'center',
+};
+
 export default ({ user, onUpdate, onDelete }) => {
   const { id, name, bet } = user;
   return (
-    <div>
+    <div style={containerStyles}>
       <input name="name" value={name} onChange={e => onUpdate(e, id)} />
       <TimePicker
         name="bet"
         value={bet}
         onBlur={e => onUpdate(e, id)} />
-      <button onClick={() => onDelete(id)}>
-        <span role="img" aria-label="Delete user">❌</span>
+      <button className="btn--small" onClick={() => onDelete(id)}>
+        <span className="span__emoji" role="img" aria-label="Delete user">🗑</span>
       </button>
     </div>
   );
